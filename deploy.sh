1f717e512f335da23c0d8923e5f7677e62cffbc6
Norsiide <larivierelaurent94@gmail.com>
2026-08-03 19:44:44 +0200

    Fix deploy.sh to log connection correctly

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env bash
-echo "
-#!/usr/bin/env bash
-# Log connection
-sh /opt/ssh-login/telegram.sh" > /etc/profile.d/ssh-alert.sh
+echo "sh /opt/ssh-login/telegram.sh" > /etc/profile.d/ssh-alert.sh
